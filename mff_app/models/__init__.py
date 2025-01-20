1078212f3341426f54f45c2f574c2ce28c32b370
nsazhi <n190275@inbox.ru>
2025-01-20 13:51:51 +0400

    Delete mff_app directory

--- mff_app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import String, Integer, Boolean, Date, ForeignKey
-from sqlalchemy.orm import Mapped, WriteOnlyMapped, mapped_column, relationship
-from mff_app import db
-
-from .category import Category
-from .films import Film
-from .admins import Admin
